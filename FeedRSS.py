e0178f8cee644a352be59b5dbbead57e495bf0ca
vadavila <vadavila@users.noreply.github.com>
2017-05-20 13:36:51 -0700

    Update FeedRSS.py

--- FeedRSS.py
+++ FeedRSS.py
@@ -1,13 +1,29 @@
+import feedparser
+import json
 def feedReader():
-    linkList = ['http://feeds.bbci.co.uk/news/rss.xml?edition=uk',
-                'http://www.reuters.com/tools/rss',
-                'http://www.foxnews.com/about/rss/',
-                'http://www.upi.com/rss/',
-                'https://www.usatoday.com/rss/',
-                'https://wildfires.einnews.com/all_rss',
-                'http://www.cnn.com/services/rss/',
-                'https://www.lib.noaa.gov/rss.html',
+    linkList = ['http://www.reuters.com/tools/rss',
+                'http://feeds.bbci.co.uk/news/rss.xml?edition=uk',
+                'http://feeds.reuters.com/news/artsculture',
+                'http://feeds.reuters.com/reuters/businessNews',
+                'http://feeds.reuters.com/reuters/companyNews',
+                'http://feeds.reuters.com/reuters/entertainment',
+                'http://feeds.reuters.com/reuters/environment',
+                'http://feeds.reuters.com/reuters/healthNews',
+                'http://feeds.reuters.com/reuters/lifestyle',
+                'http://feeds.reuters.com/news/wealth',
+                'http://feeds.reuters.com/reuters/MostRead',
+                'http://feeds.reuters.com/reuters/oddlyEnoughNews',
+                'http://feeds.reuters.com/ReutersPictures',
+                'http://feeds.reuters.com/reuters/peopleNews',
+                'http://feeds.reuters.com/Reuters/PoliticsNews',
+                'http://feeds.reuters.com/reuters/scienceNews',
+                'http://feeds.reuters.com/reuters/sportsNews',
+                'http://feeds.reuters.com/reuters/technologyNews',
+                'http://feeds.reuters.com/reuters/topNews',
+                'http://feeds.reuters.com/Reuters/domesticNews',
+                'http://feeds.reuters.com/Reuters/worldNews',
                 'http://www.aviationsafetymagazine.com/rss/']
     for link in linkList:
+        print(link)
         parsedLink = feedparser.parse(link)
-        return parsedLink['entries']
+        print(parsedLink)
